0a8005b792d834048b6f47d8184535449de52b4d
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2023-04-19 10:12:46 +0200

    feat: [IPET-1942] New possibility to send requests through localhost to avoid blocking requests by Cloudflare

--- Helper/Configuration.php
+++ Helper/Configuration.php
@@ -4,30 +4,21 @@ namespace MageSuite\ProductTileWarmup\Helper;
 
 class Configuration
 {
-    const ROUTE_TILE_WARMUP = 'tile/warmup/index';
+    const XML_PATH_GENERAL_CRAWLING_ENABLED = 'product_tile_warmup/general/crawling_enabled';
+    const XML_PATH_GENERAL_LOCALHOST_MODE_ENABLED = 'product_tile_warmup/general/localhost_mode_enabled';
+    const XML_PATH_GENERAL_DEBUG_MODE_ENABLED = 'product_tile_warmup/general/debug_mode_enabled';
+    const XML_PATH_GENERAL_CUSTOMER_GROUPS = 'product_tile_warmup/general/customer_groups';
+    const XML_PATH_GENERAL_DISABLED_STORE_VIEWS = 'product_tile_warmup/general/disabled_store_views';
+    const XML_PATH_GENERAL_WORKER_PROCESSES_CONFIGURATION = 'product_tile_warmup/general/worker_processes_configuration';
 
-    const CONFIG_PATH_ENABLED = 'product_tile_warmup/general/crawling_enabled';
-    const CONFIG_PATH_DEBUG_MODE_ENABLED = 'product_tile_warmup/general/debug_mode_enabled';
-    const CONFIG_PATH_CUSTOMER_GROUPS = 'product_tile_warmup/general/customer_groups';
-    const CONFIG_PATH_DISABLED_STORE_VIEWS = 'product_tile_warmup/general/disabled_store_views';
-    const CONFIG_PATH_WORKER_PROCESSES_CONFIGURATION = 'product_tile_warmup/general/worker_processes_configuration';
-    const CONFIG_PATH_BASIC_AUTH_USERNAME = 'product_tile_warmup/basic_auth/username';
-    const CONFIG_PATH_BASIC_AUTH_PASSWORD = 'product_tile_warmup/basic_auth/password';
+    const XML_PATH_BASIC_AUTH_USERNAME = 'product_tile_warmup/basic_auth/username';
+    const XML_PATH_BASIC_AUTH_PASSWORD = 'product_tile_warmup/basic_auth/password';
 
-    /**
-     * @var \Magento\Framework\App\Config\ScopeConfigInterface
-     */
-    protected $scopeConfig;
+    protected \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig;
 
-    /**
-     * @var \Magento\Framework\Serialize\SerializerInterface
-     */
-    protected $serializer;
+    protected \Magento\Framework\Serialize\SerializerInterface $serializer;
 
-    /**
-     * @var \Magento\Framework\Encryption\EncryptorInterface
-     */
-    protected $encryptor;
+    protected \Magento\Framework\Encryption\EncryptorInterface $encryptor;
 
     public function __construct(
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
@@ -41,17 +32,22 @@ class Configuration
 
     public function isEnabled(): bool
     {
-        return (bool)$this->scopeConfig->getValue(self::CONFIG_PATH_ENABLED);
+        return (bool)$this->scopeConfig->getValue(self::XML_PATH_GENERAL_CRAWLING_ENABLED);
+    }
+
+    public function isLocalhostModeEnabled(): bool
+    {
+        return (bool)$this->scopeConfig->getValue(self::XML_PATH_GENERAL_LOCALHOST_MODE_ENABLED);
     }
 
     public function isDebugModeEnabled(): bool
     {
-        return (bool)$this->scopeConfig->getValue(self::CONFIG_PATH_DEBUG_MODE_ENABLED);
+        return (bool)$this->scopeConfig->getValue(self::XML_PATH_GENERAL_DEBUG_MODE_ENABLED);
     }
 
     public function getDisabledStoreViewIds(): array
     {
-        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_DISABLED_STORE_VIEWS);
+        $value = $this->scopeConfig->getValue(self::XML_PATH_GENERAL_DISABLED_STORE_VIEWS);
 
         if ($value === null) {
             return [];
@@ -62,7 +58,7 @@ class Configuration
 
     public function getCustomerGroupIds(): array
     {
-        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_CUSTOMER_GROUPS);
+        $value = $this->scopeConfig->getValue(self::XML_PATH_GENERAL_CUSTOMER_GROUPS);
 
         if ($value === null) {
             return [];
@@ -73,7 +69,7 @@ class Configuration
 
     public function getWorkerProcessesConfiguration(): array
     {
-        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_WORKER_PROCESSES_CONFIGURATION);
+        $value = $this->scopeConfig->getValue(self::XML_PATH_GENERAL_WORKER_PROCESSES_CONFIGURATION);
 
         if ($value === null) {
             return [];
@@ -84,12 +80,12 @@ class Configuration
 
     public function getBasicAuthUsername(): ?string
     {
-        return $this->scopeConfig->getValue(self::CONFIG_PATH_BASIC_AUTH_USERNAME);
+        return $this->scopeConfig->getValue(self::XML_PATH_BASIC_AUTH_USERNAME);
     }
 
     public function getBasicAuthPassword(): ?string
     {
-        $value = $this->scopeConfig->getValue(self::CONFIG_PATH_BASIC_AUTH_PASSWORD);
+        $value = $this->scopeConfig->getValue(self::XML_PATH_BASIC_AUTH_PASSWORD);
 
         if (!empty($value)) {
             return $this->encryptor->decrypt($value);
